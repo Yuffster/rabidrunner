67ad0e36cbcd75ead8c5384c2ae1bb3cb5b1692d
Yuffster <msteigerwalt@gmail.com>
2009-01-03 07:28:56 +0000

    Preliminary code for XML, JSON, JSON-P support.  (Untested.)

--- runner.php
+++ runner.php
@@ -3,27 +3,62 @@
 	include('Test.php');
 	$runner = new TestRunner();
 	$results = $runner->runTests();
+	$f = $_GET['format'];
 ?>
-
-<? foreach ($results as $test): ?>
-
-	<h2>
-		<?= $test->name; ?>:
-		<?= $test->passing + $test->expectedFailures ?>/<?=$test->passing+$test->failing?>
-		<?if ($test->expectedFailures): ?>
-			(<?=$test->expectedFailures?> expected failures)
-		<?endif;?>
-	</h2>
-	<ol>
-		<?foreach ($test->results as $name=>$data):?>
-			<li class="<?= ($data['result']) ? 'passed' : 'failed';?>">
-				<strong><?= ($data['result']) ? 'PASS' : 'FAIL' ?></strong>:
-				<?= ucfirst(str_replace('_', ' ', $name)); ?>.
-				<? if ($data['message']): ?>
-					<span class="message">(<?= $data['message'] ?>)</span>
-				<?endif;?>
-			</li>
+<? if ($f == 'xml'): header("Content-Type: text/xml");?>
+<?="<?"?>xml version="1.0" encoding="UTF-8"?>
+	<results>
+		<?foreach ($results as $test) : ?>
+			<set title="<?=$test->name?>">
+				<failing expected="<?=$test->expectedFailures?>"><?=$test->failing?></failing>
+				<passing><?=$test->passing?></passing>
+				<specs>
+					<?foreach ($test->results as $name=>$data) : ?>
+						<spec>
+							<name><?=$name?></name>
+							<result><?=$data['result']?></result>
+							<message><?=$data['message']?></message>
+						</spec>
+					<?endforeach;?>
+				</specs>
+			</set>
 		<?endforeach;?>
-	</ol>
-
-<?endforeach;?>
+	</results>
+<? elseif ($f == 'json' OR 'jsonp'):
+	header("Content-Type: text/javascript");
+	$return = Array();
+	foreach ($results as $test):
+		$return[] = Array('title'=>$test->name,
+	                    'expectedFailures'=>$test->expectedFailures,
+		    		    'passing'=>$test->passing,
+						'failing'=>$test->failing,
+		                'results'=>$test->results);
+	endforeach;
+	$json = json_encode($return);
+	$id = $_GET['id'];
+	if ($f == 'jsonp') echo "rabidrunner_callback[$_GET[id]](";
+	echo $json;
+	if ($f == 'jsonp') echo ");";
+?>
+<? else: ?>
+	<? foreach ($results as $test): ?>
+		<h2>
+			<?= $test->name; ?>:
+			<?= $test->passing + $test->expectedFailures ?>/<?=$test->passing+$test->failing?>
+			<?if ($test->expectedFailures): ?>
+				(<?=$test->expectedFailures?> expected failures)
+			<?endif;?>
+		</h2>
+		<ol>
+			<?foreach ($test->results as $name=>$data):?>
+				<li class="<?= ($data['result']) ? 'passed' : 'failed';?>">
+					<strong><?= ($data['result']) ? 'PASS' : 'FAIL' ?></strong>:
+					<?= ucfirst(str_replace('_', ' ', $name)); ?>.
+					<? if ($data['message']): ?>
+						<span class="message">(<?= $data['message'] ?>)</span>
+					<?endif;?>
+				</li>
+			<?endforeach;?>
+		</ol>
+	<?endforeach;?>
+<?endif;?>
